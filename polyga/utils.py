0c6b04ed473eab984935aacc6cde889d40b20ba4
jdkern11 <leolion29@gmail.com>
2021-09-01 20:32:19 -0400

    add testing feature

--- polyga/utils.py
+++ polyga/utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import itertools
 
 import numpy as np
 import pandas as pd
---
+++ tests/test_polyga.py
@@ -0,0 +1,28 @@
+import pytest
+import shutil
+import os
+import pickle
+
+from polyga import polygod as pg
+from polyga import utils 
+
+def nothing():
+    print(test) 
+
+def test_initialize():
+    planet = pg.PolyPlanet('Planet_Silly', 
+            predict_function=nothing,
+            fingerprint_function=nothing,
+            )
+
+    land = pg.PolyLand('Awesomeland', planet, 
+            generative_function=utils.chromosome_ids_to_smiles,
+            fitness_function=nothing
+            )
+
+    nation = pg.PolyNation('UnitedPolymersOfCool', land, selection_scheme='elite', 
+                           partner_selection='diversity', 
+                           num_population_initial=180,
+                           )
+    planet.complete_run()
+    shutil.rmtree('Planet_Silly')
